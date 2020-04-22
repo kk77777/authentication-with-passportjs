ad8e91f1baf783455ae3da17edb21b9390e15ed8
kk77777 <kaushikgattani@gmail.com>
2020-04-22 11:36:21 +0530

    setting views for ejs

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use('/', require('./routes/index'));
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
+//Routes
+app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
 app.listen(PORT, console.log(`Connected to server on port ${PORT}`));
