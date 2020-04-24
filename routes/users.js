a8fac3b0948b6a5e69dcaa30bd3ec077569ae423
kk77777 <kaushikgattani@gmail.com>
2020-04-24 07:43:52 +0530

    ui for login.ejs

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', (req, res) => {
-  res.send('Login');
+  res.render('login');
 });
 
 router.get('/register', (req, res) => {
