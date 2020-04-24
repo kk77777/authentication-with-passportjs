21696f57f1559b30ac8d99840d8004272c91a03c
kk77777 <kaushikgattani@gmail.com>
2020-04-24 09:00:30 +0530

    register form validation

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,37 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 
+//Handle Register
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  //Check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please dont leave any field empty!' });
+  }
+
+  //Check passwords match
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  //Check password length
+  if (password.length < 6) {
+    errors.push({ msg: 'Password length should be 6 characters or more' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    res.send('Passed');
+  }
+});
+
 module.exports = router;
